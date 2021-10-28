75e09c109d25f9f4f77a617a6e7b8217cfcbde9a
kchrobak44 <kchrobak04@gmail.com>
2021-10-28 17:54:58 +0200

    Initial commit

---
+++ src/test/java/Karol/Chrobak/student/list/StudentListApplicationTests.java
@@ -0,0 +1,13 @@
+package Karol.Chrobak.student.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
